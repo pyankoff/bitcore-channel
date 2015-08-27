2a25a487ae496deed89bda7a7f7e6871449725f7
pyankoff <pyankoff@gmail.com>
2015-08-27 08:32:56 +0700

    version change

--- package.js
+++ package.js
@@ -2,12 +2,12 @@ Package.describe({
   name: 'pyankoff:bitcore-channel',
   version: '1.0.0',
   summary: 'Payment channels smart contract support for bitcore.',
-  git: 'https://github.com/pyankoff/meteor-packages.git'
+  git: 'https://github.com/pyankoff/bitcore-channel.git'
 });
 
 Npm.depends({'bitcore-channel': '0.12.0'});
 
 Package.onUse(function(api) {
-  api.export('bitcore-channel');
-  api.addFiles('bitcore-channel.js', 'server');
+  api.export('bitcoreChannel');
+  api.addFiles('bitcore-channel.js', 'client');
 });
